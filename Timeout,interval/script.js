f47968973a0175e0badc9c6fd9a58f424cb46da3
vyshnavi <vishuchowdary20@gmail.com>
2025-07-21 10:27:54 +0530

    clearTimeout

--- Timeout,interval/script.js
+++ Timeout,interval/script.js
@@ -1,7 +1,23 @@
 //set timeout
 //after a specific time it execute
 
+/*
 function sayHello(){
     console.log("hello vyshnavi");
 }
 setTimeout(sayHello,2000);
+*/
+
+/*
+setTimeout(function(){
+    console.log("anonymous function as callback")
+},2000);
+*/
+
+//clearTimeout
+
+const TimeoutId = setTimeout(function(){
+    console.log("hello vyshu");
+},5000);
+
+clearTimeout(TimeoutId);
