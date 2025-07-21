595ce7e091a6959998a7a91e932b4273511be316
vyshnavi <vishuchowdary20@gmail.com>
2025-07-21 13:38:44 +0530

    debugg

--- base copy/script.js
+++ base copy/script.js
@@ -1,4 +1,5 @@
  
+ 
  console.log(x); //x is not defined
  var x=5;
  console.log(x);
@@ -13,3 +14,26 @@
  }
 
  //we an declare var and function at top of scope
+
+ 
+ var y = "helllo";
+ if (y!=="hello"){
+    console.warn("x is not equal to hello");
+
+ }
+    
+
+ var z = undefined;
+ if(z === undefined){
+    console.error("z is not defined");
+ }
+    
+
+ var users = [
+    {name:"vyshu",age:23,city:"nellore"},
+    {name:"ruchi",age:21,city:"thirupathi"},
+    {name:"teju",age:20,city:"sulurpeta"},
+    {name:"meghana",age:22,city:"podalakur"}
+ ];
+
+ console.table(users);
