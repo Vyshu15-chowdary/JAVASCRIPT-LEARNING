9f87c0c0d5a983400ec36cdf894a7c49b347aaea
vyshnavi <vishuchowdary20@gmail.com>
2025-07-21 08:22:46 +0530

    Pop up boxes

--- Alert/script.js
+++ Alert/script.js
@@ -2,3 +2,12 @@ alert ('welcome to javascript course 🧑‍🎓')
 
 //global object it contains many methods
 console.log(window)
+
+//confirm box
+
+confirm("Are you sure to exit");
+
+//prompt Box
+
+let ans = prompt("what is your name?");
+console.log(ans);
