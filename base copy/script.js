1eaa5feda1c80a0a0fcf65ab7cef7e7ada94fee9
vyshnavi <vishuchowdary20@gmail.com>
2025-07-21 12:14:39 +0530

    hoisting

---
+++ base copy/script.js
@@ -0,0 +1,15 @@
+ 
+ console.log(x); //x is not defined
+ var x=5;
+ console.log(x);
+ 
+
+ console.log(x);
+ let x;//error:cannot acess before intialization
+
+ foo();
+ function foo(){
+    console.log("iam a hoisted function")
+ }
+
+ //we an declare var and function at top of scope
