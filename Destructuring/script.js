51a675c41cdb60acb0be3b42f01a1c8660acec58
vyshnavi <vishuchowdary20@gmail.com>
2025-07-23 21:24:49 +0530

    destructuring-array

---
+++ Destructuring/script.js
@@ -0,0 +1,11 @@
+//destructuring-Array
+
+const numbers = [1,2,3,4,5];
+
+//destructuring array into individual variables
+
+const [a,b,c,d,e] = numbers;
+console.log(a);
+console.log(b);
+console.log(e);
+
