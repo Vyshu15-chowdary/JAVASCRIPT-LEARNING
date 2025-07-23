b64099a5286be46428904380c3b1c4777b212c83
vyshnavi <vishuchowdary20@gmail.com>
2025-07-23 21:29:19 +0530

    destructured reversed array

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -9,3 +9,12 @@ console.log(a);
 console.log(b);
 console.log(e);
 
+// reverse values using destructuring
+
+const Digits =[1,2,3,4];
+Digits.reverse();
+//destructure reversed array
+
+const [four,three,two,one] = Digits;
+console.log(one);
+console.log(four);
