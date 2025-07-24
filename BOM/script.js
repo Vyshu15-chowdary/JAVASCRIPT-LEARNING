b9ddcfbdfb1d7a40944245b6d40c3f039b6bc4e3
vyshnavi <vishuchowdary20@gmail.com>
2025-07-24 18:52:22 +0530

    navigator object

--- BOM/script.js
+++ BOM/script.js
@@ -27,3 +27,31 @@ console.log(window.history.go(2));
 
 console.log("current state :" ,window.history.state ); //null
 
+// navigator object
+
+//getnavigator object
+const nav = window.navigator;
+console.log(nav);
+
+//get name of browser
+const browserName = nav.appName;
+console.log(browserName); //netscape
+
+//version of browser
+
+const browserVersion = nav.appVersion;
+console.log(browserVersion); //5.0
+
+// user agent string
+
+const userAgent =nav.userAgent;
+console.log(userAgent); //Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0
+
+//lang of browser
+
+const browserLang = nav.language;
+console.log(browserLang); //en-us
+
+//os of user
+const Os = nav.platform;
+console.log(Os); //win 32
