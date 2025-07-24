68bb60a7fb5659326614b1cd67343f0da80d1d75
vyshnavi <vishuchowdary20@gmail.com>
2025-07-24 11:33:14 +0530

    assigning values using s.o

--- Spreadoperator/script.js
+++ Spreadoperator/script.js
@@ -4,3 +4,9 @@ var arr1 =[1,2,3];
 var arr2 = [4,5,6];
 var arr3 = [...arr1,...arr2];
 console.log(arr3);
+
+//assign values using S.O
+
+var obj1 ={a:1,b:2};
+var obj2 = {...obj1,c:3,d:4};
+console.log(obj2);
