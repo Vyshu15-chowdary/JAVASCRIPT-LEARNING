a824e748c2b618d6801b0581d07e1c9067125eac
vyshnavi <vishuchowdary20@gmail.com>
2025-07-24 16:50:37 +0530

    enhanced object literal

---
+++ EnhancedObjectliteral/script.js
@@ -0,0 +1,21 @@
+//without EOL
+
+var x = 10;
+var y = 20;
+
+var obj = {
+    x:x,
+    y:y
+};
+console.log(obj);
+
+// with EOL
+
+var x =30;
+var y = 40;
+
+var obj ={
+    x,
+    y
+}
+console.log(obj);
