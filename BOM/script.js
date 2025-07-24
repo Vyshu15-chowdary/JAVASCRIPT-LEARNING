0a4babf10828b0bd7eb00005e3a3db4aaccc3d49
vyshnavi <vishuchowdary20@gmail.com>
2025-07-24 18:30:52 +0530

    window object

---
+++ BOM/script.js
@@ -0,0 +1,6 @@
+console.log(window.document.title);
+window.alert("Hello World!"); // displays "Hello World!" in an alert box
+
+console.log(window.history.length); //1
+console.log(window.location.href); //http://127.0.0.1:5500/BOM/index.html
+console.log(window.navigator.userAgent); //Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0
