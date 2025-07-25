bd216c052b5a12f2571c0286fa313a53f3473ba8
vyshnavi <vishuchowdary20@gmail.com>
2025-07-25 10:20:49 +0530

    JSON parse

--- JSON-XML/script.js
+++ JSON-XML/script.js
@@ -7,3 +7,8 @@ const Json =
     "Movies":["chandramukhi","robo","robo2"]
 };
 console.log(Json);
+
+//json parse
+const jsonString = '{"name":"Johnsmith","age":40,"isStudent":true,"favouriteFoods":["pizza","kfc","biryani"]}';
+const jsonObj = JSON.parse(jsonString);
+console.log(jsonObj.favouriteFoods);//['pizza', 'kfc', 'biryani']
