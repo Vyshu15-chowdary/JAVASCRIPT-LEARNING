c3b8047704257a5b7639b7e0a61db075377f2813
vyshnavi <vishuchowdary20@gmail.com>
2025-07-25 10:28:26 +0530

    JSON.stringyfy

--- JSON-XML/script.js
+++ JSON-XML/script.js
@@ -12,3 +12,11 @@ console.log(Json);
 const jsonString = '{"name":"Johnsmith","age":40,"isStudent":true,"favouriteFoods":["pizza","kfc","biryani"]}';
 const jsonObj = JSON.parse(jsonString);
 console.log(jsonObj.favouriteFoods);//['pizza', 'kfc', 'biryani']
+
+//JSON.stringyfy
+
+//j.s object to json string
+
+const obj = { name: "John Smith", age: 30, isStudent: true, favoriteFoods: ["pizza", "tacos", "sushi"] };
+const jsonStringfy = JSON.stringify(obj);
+console.log(jsonString); // output: {"name":"John Smith","age":30,"isStudent":true,"favoriteFoods":["pizza","tacos","sushi"]}
