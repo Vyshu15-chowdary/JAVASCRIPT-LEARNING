d23746b786a4a9ef03aeeb9712f00eaacf9030a5
vyshnavi <vishuchowdary20@gmail.com>
2025-07-25 10:43:29 +0530

    XMLSerializer object

--- JSON-XML/script.js
+++ JSON-XML/script.js
@@ -23,6 +23,7 @@ console.log(jsonString); // output: {"name":"John Smith","age":30,"isStudent":tr
 
 //XML
 
+
 const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
 <bookstore>
   <book category="Science Fiction">
@@ -37,6 +38,42 @@ const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
   </book>
 </bookstore>`;
 
+
 const parser = new DOMParser();
 const xmlDoc = parser.parseFromString(xmlString, "text/xml");
 console.log(xmlDoc.getElementsByTagName("book")[0].getAttribute("category")); // output: Science Fiction
+
+
+
+//J.s object to XML string
+
+const books = [
+  {category: "Science Fiction", title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", price: 9.99},
+  {category: "Mystery", title: "The Da Vinci Code", author: "Dan Brown", price: 15.99}
+];
+
+const xmlDoc = document.createElement("bookstore");
+
+books.forEach(book => {
+  const bookElem = document.createElement("book");
+  bookElem.setAttribute("category", book.category);
+  
+  const titleElem = document.createElement("title");
+  titleElem.textContent = book.title;
+  
+  const authorElem = document.createElement("author");
+  authorElem.textContent = book.author;
+  
+  const priceElem = document.createElement("price");
+  priceElem.textContent = book.price;
+  
+  bookElem.appendChild(titleElem);
+  bookElem.appendChild(authorElem);
+  bookElem.appendChild(priceElem);
+  
+  xmlDoc.appendChild(bookElem);
+});
+
+const serializer = new XMLSerializer();
+const xmlString = serializer.serializeToString(xmlDoc);
+console.log(xmlString);
