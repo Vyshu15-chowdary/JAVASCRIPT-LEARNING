bd3e705c0e2be36771f0765dc2bd35a7e5acaabf
vyshnavi <vishuchowdary20@gmail.com>
2025-07-25 18:31:38 +0530

    encapsulation

---
+++ OOPs/script.js
@@ -0,0 +1,27 @@
+//encapsulation
+
+/*
+class MyClass {
+    constructor(){
+        this._myProtectedProperty = 'some value';
+    }
+}
+    */
+
+class MyClass{
+    #privateField = "private"; //only acessed within class
+
+    publicMethod(){
+        console.log(this.privateField);
+    }
+}
+
+class PClass {
+  #privateMethod() {
+    console.log("private method");
+  }
+
+  publicMethod() {
+    this.#privateMethod();
+  }
+}
