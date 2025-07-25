263b33beb12e40be5ee65ab4e083e31a8920c722
vyshnavi <vishuchowdary20@gmail.com>
2025-07-25 10:33:04 +0530

    XML

--- JSON-XML/script.js
+++ JSON-XML/script.js
@@ -20,3 +20,23 @@ console.log(jsonObj.favouriteFoods);//['pizza', 'kfc', 'biryani']
 const obj = { name: "John Smith", age: 30, isStudent: true, favoriteFoods: ["pizza", "tacos", "sushi"] };
 const jsonStringfy = JSON.stringify(obj);
 console.log(jsonString); // output: {"name":"John Smith","age":30,"isStudent":true,"favoriteFoods":["pizza","tacos","sushi"]}
+
+//XML
+
+const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
+<bookstore>
+  <book category="Science Fiction">
+    <title>The Hitchhiker's Guide to the Galaxy</title>
+    <author>Douglas Adams</author>
+    <price>9.99</price>
+  </book>
+  <book category="Mystery">
+    <title>The Da Vinci Code</title>
+    <author>Dan Brown</author>
+    <price>15.99</price>
+  </book>
+</bookstore>`;
+
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+console.log(xmlDoc.getElementsByTagName("book")[0].getAttribute("category")); // output: Science Fiction
