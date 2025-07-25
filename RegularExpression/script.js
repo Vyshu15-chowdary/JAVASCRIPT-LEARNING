92a7d903e023bc662eb22477b998b74dfca5146c
vyshnavi <vishuchowdary20@gmail.com>
2025-07-25 09:48:12 +0530

    RegExp literal

--- RegularExpression/script.js
+++ RegularExpression/script.js
@@ -2,3 +2,13 @@
 //it test if a string contains specific substring
 const regex1 = new RegExp("hello");
 console.log(regex1.test("hello world"));//true
+
+//using regexp literal
+
+const regex2 = /world/; //literals are direct represantation of a value in source code.fixed value
+
+console.log(regex2.test("hello")); //false
+
+
+
+
